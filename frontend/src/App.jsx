24e5f39f2e63d68a2211d67d24d1a396bb6b653f
g_zawie <gzawie24@gmail.com>
2025-12-14 20:50:48 +0300

    Vite + Tailwind portfolio setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-5xl font-bold text-pink-500">
+        Tailwind v4 + Vite is working 💖
+      </h1>
+    </div>
+  )
+}
+
+export default App
